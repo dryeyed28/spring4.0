d9b93c858ca0cad736206b1f84575df80f6eebbd
dryeyed28 <dryeyed28@naver.com>
2018-06-17 18:16:20 +0900

    스프링 4.0 예제

---
+++ spring4-chap01/src/test/java/net/madvirus/spring4/AppTest.java
@@ -0,0 +1,20 @@
+package net.madvirus.spring4;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
